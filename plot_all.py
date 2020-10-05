65cb80518b046a04c5b06f1d767133d6c3fdd304
myMyth0211 <71620269+myMyth0211@users.noreply.github.com>
2020-10-05 08:55:19 +0800

    Update plot_all.py

--- plot_all.py
+++ plot_all.py
@@ -49,12 +49,12 @@ def get_data(person="S1", ch="B", turn=1):
         cols = train_data.col_values(j)
         train_target[:, j] = cols[event_target[0, 1] - 1:event_target[11, 1] + 150]
 
-    return train_target
+    return train_target,event_target
 
 
 # 函数入口，用于绘制指定测试者的相应字符的各轮脑电图
 def main(person="S1", ch="B", turn=1):
-    train = get_data(person, ch, turn)  # 获取脑电波数据
+    train,_ = get_data(person, ch, turn)  # 获取脑电波数据
 
     # 数据可视化，绘制20个通道的折线图
     plt.plot(train)
